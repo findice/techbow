ca4ca2b51554330261cf1c979f3ebf93a0bd505b
iyangzeshi <iyangzeshi@outlook.com>
2020-12-20 00:58:04 -0800

    origin

--- src/algorithm_and_data_structure_basic/session16/Q01_2DeepCopyTree.java
+++ src/algorithm_and_data_structure_basic/session16/Q01_2DeepCopyTree.java
@@ -1,7 +1,9 @@
 package algorithm_and_data_structure_basic.session16;
 
+import dependentClass.TreeNode;
+
 /*deep copy a tree*/
-public class Session16Q01_2 {
+public class Q01_2DeepCopyTree {
 	public static TreeNode deepCopyTree(TreeNode root) {
 		// corner&base case
 		if (root == null) {
@@ -13,23 +15,5 @@ public class Session16Q01_2 {
 		newRoot.left = deepCopyTree(root.left);
 		newRoot.right = deepCopyTree(root.right);
 		return newRoot;
-	}
-}
-
-class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
-
-	public TreeNode(int val) {
-		this.val = val;
-		this.left = null;
-		this.right = null;
-	}
-
-	public TreeNode(int val, TreeNode left, TreeNode right) {
-		this.val = val;
-		this.left = left;
-		this.right = right;
 	}
 }
