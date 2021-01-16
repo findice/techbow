817a399bd2c7d8aa9a3272b6bf0ed0aea9f0c9a6
iyangzeshi <iyangzeshi@outlook.com>
2021-01-15 21:49:44 -0800

    origin

--- src/algorithm_and_data_structure_basic/session13DP/FindLargestSquareSurroundedBy1.java
+++ src/algorithm_and_data_structure_basic/session13DP/FindLargestSquareSurroundedBy1.java
@@ -7,31 +7,36 @@ package algorithm_and_data_structure_basic.session13DP;
 //Date: 2021-01-15 星期五 10:51
 
 /*
-mat[6][6] =  1  0  1  1  1  1
-             1  0  1  1  0  1
-             1  1  1  0  0  1
-             0  1  1  1  1  1
-             1  1  1  0  1  0
-             0  0  1  0  0  0
-hor[i][j] means how many consecutive 1 from (i, j) to left
-hor[6][6] = 1  0  1  2  3  4
-            1  0  1  2  0  1
-            1  2  3  0  0  1
-            0  1  2  3  4  5
-            1  2  3  0  1  0
+mat[6][6] = 1  0  1  1  1  1
+            1  0  1  1  0  1
+            1  1  1  0  0  1
+            0  1  1  1  1  1
+            1  1  1  0  1  0
             0  0  1  0  0  0
-ver[i][j] means how many consecutive 1 from (i, j) to up
-ver[6][6] = 1  0  1  1  1  1
-            2  0  2  2  0  2
-            3  1  3  0  0  3
-            0  2  4  1  1  4
-            1  3  5  0  2  0
-            0  0  6  0  0  0
+
+right[i][j] means how many consecutive 1 from (i, j) to left
+right[6][6] =
+            1  0  4  3  2  1
+            1  0  2  1  0  1
+            3  2  1  0  0  1
+            0  5  4  3  2  1
+            3  2  1  0  1  0
+            0  0  1  0  0  0
+
+down[i][j] means how many consecutive 1 from (i, j) to up
+down[6][6] =
+            3  0  6  1  1  4
+            2  0  5  2  0  3
+            1  3  4  0  0  2
+            0  2  3  1  2  1
+            1  1  2  0  1  0
+            0  0  1  0  0  0
  */
 
 /*
 找最大的正方形，可以空心
  */
+// T(m, n) = O(m * n * min(m, n)), S(m,n) = O(m * n)
 class FindLargestSquareSurroundedBy1 {
     
     public static void main(String[] args) {
@@ -54,22 +59,23 @@ class FindLargestSquareSurroundedBy1 {
         int cols = matrix[0].length;
         int[][] right = new int[rows][cols];
         int[][] down = new int[rows][cols];
-        
+    
         // get right and down by dp
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] != 0) {
-                    if (i == 0) {
-                        down[i][j] = 1;
-                    } else {
-                        down[i][j] = down[i - 1][j] + 1;
-                    }
-                    
-                    if (j == 0) {
-                        right[i][j] = 1;
-                    } else {
-                        right[i][j] = right[i][j - 1] + 1;
-                    }
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 0; j--) {
+                if (matrix[i][j] == 0) {
+                    continue;
+                }
+                if (i == rows - 1) {
+                    down[i][j] = 1;
+                } else {
+                    down[i][j] = down[i + 1][j] + 1;
+                }
+            
+                if (j == cols - 1) {
+                    right[i][j] = 1;
+                } else {
+                    right[i][j] = right[i][j + 1] + 1;
                 }
             }
         }
@@ -81,7 +87,7 @@ class FindLargestSquareSurroundedBy1 {
             for (int j = 0; j < cols; j++) {
                 int len = Math.min(right[i][j], down[i][j]);
                 while (len > 0) {
-                    boolean isSquare = down[i][j - len + 1] >= len && right[i - len + 1][j] >= len;
+                    boolean isSquare = down[i][j + len - 1] >= len && right[i + len - 1][j] >= len;
                     if (isSquare && maxLen < len) {
                         maxLen = len;
                     }
