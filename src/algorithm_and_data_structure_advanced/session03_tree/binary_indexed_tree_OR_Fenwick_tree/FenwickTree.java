dff2a9ba29a765cd70d8b6388a50966851cd119e
iyangzeshi <iyangzeshi@outlook.com>
2021-03-04 12:42:23 -0800

    origin

--- src/algorithm_and_data_structure_advanced/session03_tree/binary_indexed_tree_OR_Fenwick_tree/FenwickTree.java
+++ src/algorithm_and_data_structure_advanced/session03_tree/binary_indexed_tree_OR_Fenwick_tree/FenwickTree.java
@@ -9,26 +9,26 @@ package algorithm_and_data_structure_advanced.session03_tree.binary_indexed_tree
 // fenwick tree又叫binary indexed tree，因为这个数据结构是前者发明的
 public class FenwickTree {
     
-    private final int[] preSum;
+    private final int[] sums;
     
     public FenwickTree(int n) {
-        preSum = new int[n + 1];
+        sums = new int[n + 1];
     }
     
     /** return sum from array[1] to array[index] (inclusively), T(n) = O(lg(n))*/
     public int query(int index) {
         int res = 0;
         for (int i = index; i > 0; i -= lowbit(i)) { // 边界是 i > 0
-            res += preSum[i];
+            res += sums[i];
         }
         return res;
     }
     
     /** add delta to array[index], T(n) = O(lg(n))*/
     public void update(int index, int delta) {
-        int len = preSum.length;
+        int len = sums.length;
         for (int i = index; i < len; i += lowbit(i)) { // 与查询相反, 边界是 i < len
-            preSum[i] += delta;
+            sums[i] += delta;
         }
     }
     
