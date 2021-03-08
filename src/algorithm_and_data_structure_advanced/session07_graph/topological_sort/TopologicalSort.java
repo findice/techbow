37177c29cc036a1d737eee2ab761daa6fddfd2ac
iyangzeshi <iyangzeshi@outlook.com>
2021-03-08 14:15:48 -0800

    origin

--- src/algorithm_and_data_structure_advanced/session07_graph/topological_sort/TopologicalSort.java
+++ src/algorithm_and_data_structure_advanced/session07_graph/topological_sort/TopologicalSort.java
@@ -40,13 +40,20 @@ public class TopologicalSort {
         }
         
         cur.status = Status.VISITING;
+        // res.add(cur); // part 1
         for (Vertex next : graph.get(cur)) {
             if (dfs(next, graph, res)) {
                 return true;
             }
         }
         cur.status = Status.VISITED;
-        res.addFirst(cur);
+        res.addFirst(cur);// part 2
+        /*
+        必须用part2，不能用part1
+        因为是一个图，所有的，比如C依赖于A和B，
+        part2可以实现，所有A和B都必须在C之前加入
+        part1不能，只能实现A或者B有一个在C之前，剩下的不能保证放在C之前
+         */
         return false;
     }
     
